4961705d0d8d99b3cb6ed6bc1143502ab2458fc0
Jeppe <jepp026b@edu.zealand.dk>
2024-05-07 14:39:46 +0200

    gg

--- ga_snake.py
+++ ga_snake.py
@@ -1,15 +1,67 @@
 #!/usr/bin/env python
 
 
+from numpy import number
 from snake import SnakeGame
 from ga_controller import GAController
 
-def kill_half(sorted_list):
+def remove_lowest_values(sorted_list):
     # Calculate the index to start slicing from
     index_to_remove = len(sorted_list) // 2
     # Slice the list to remove the lowest values
     result = sorted_list[index_to_remove:]
     return result
+
+def init_population(pop_size):
+    population = []
+    for _ in range(pop_size):
+        game = SnakeGame()
+        controller = GAController()
+    return (game, controller)
+
+class Population:
+    def __init__(self, population_size: int, generations: number):
+        self.generations = generations
+        self.population_size = population_size
+        self.population = init_population(population_size)
+
+    def mutate_pop(self):
+        for pop in self.population:
+            pop.game.mutate(0.05)
+    def selection(self):
+        self.population
+        selected = []
+        results = []
+        for population_tuple in self.population:
+            game = population_tuple[0]
+            obj_controller = population_tuple[1]
+            controller = obj_controller(game)
+            game.run()
+            results.append(controller)
+
+        results = remove_lowest_values(results)
+        return results
+    def mate_in_pairs(self):     # Extracting the list of keys to access the creatures in order    keys = list(creatures_dict.keys())          # Iterate over the keys in steps of 2 to get pairsfor i in range(0, len(keys) - 1, 2):         creature1 = creatures_dict[keys[i]]         creature2 = creatures_dict[keys[i+1]]         creature1.mate(creature2)
+        babies = []
+        for i in range(0, len(self.population) - 1, 2):
+            dad = self.population[i]
+            mom = self.population[i+1]
+            baby = dad + mom
+            baby2 = dad + mom
+            baby.mutate(0.05)
+            baby2.mutate(0.05)
+            babies.append(baby,baby2)
+        return babies
+
+    #         population.append(controller)
+
+pop = Population(2_000, 100)
+while pop.generations < 100:
+    pop.mutate_pop()
+    pop.selection()
+    pop.mate_in_pairs()
+
+
 
 
 
@@ -22,40 +74,35 @@ def get_fitness(game):
 
 # Run each game in the population
 
-def mate_in_pairs(population):     # Extracting the list of keys to access the creatures in order    keys = list(creatures_dict.keys())          # Iterate over the keys in steps of 2 to get pairsfor i in range(0, len(keys) - 1, 2):         creature1 = creatures_dict[keys[i]]         creature2 = creatures_dict[keys[i+1]]         creature1.mate(creature2)
-    babies = []
-    for i in range(0, len(population) - 1, 2):
-        dad = population[i]
-        mom = population[i+1]
-        baby = dad + mom
-        baby2 = dad + mom
-        baby.mutate(0.05)
-        baby2.mutate(0.05)
-        babies.append(baby,baby2)
-    return babies
-
-
-def init_population(pop_size):
-    population = []
-    for _ in range(pop_size):
-        game = SnakeGame()
-        population.append(game)
-    return population
-
-def generations(generations, pop_size):
-    gens = []
-    init_population(pop_size)
+# def mate_in_pairs(population):     # Extracting the list of keys to access the creatures in order    keys = list(creatures_dict.keys())          # Iterate over the keys in steps of 2 to get pairsfor i in range(0, len(keys) - 1, 2):         creature1 = creatures_dict[keys[i]]         creature2 = creatures_dict[keys[i+1]]         creature1.mate(creature2)
+#     babies = []
+#     for i in range(0, len(population) - 1, 2):
+#         dad = population[i]
+#         mom = population[i+1]
+#         baby = dad + mom
+#         baby2 = dad + mom
+#         baby.mutate(0.05)
+#         baby2.mutate(0.05)
+#         babies.append(baby,baby2)
+#     return babies
 
 
 
-    for generation in range(generations):
-        for game in generation:
-            controller = GAController(game)
-            game.run()
-            population.append(controller)
-        parents = kill_half(population)
-        offspring = mate_in_pairs(parents)
-        population = parents.append(offspring)
+
+# def generations(generations, pop_size):
+#     gens = []
+#     init_population(pop_size)
+
+
+
+    # for generation in range(generations):
+    #     for game in generation:
+    #         controller = GAController(game)
+    #         game.run()
+    #         population.append(controller)
+    #     parents = kill_half(population)
+    #     offspring = mate_in_pairs(parents)
+    #     population = parents.append(offspring)
 
 
 generations(100, 100)
