15bc6101aa5a35afd8f59571ebb5af5ff669ce84
Jeppe Østergaard Marquardt <jepp026b@stud.kea.dk>
2024-05-17 19:01:19 +0200

    added evolve generations with tournament, but it does not work as intended

--- ga_snake.py
+++ ga_snake.py
@@ -1,35 +1,9 @@
 # #!/usr/bin/env python
 
+import random
 from snake import SnakeGame
 from ga_controller import GAController
 
-# def kill_half(sorted_list):
-#     # Calculate the index to start slicing from
-#     sorted()
-#     index_to_remove = len(sorted_list) // 2
-#     # Slice the list to remove the lowest values
-#     result = sorted_list[index_to_remove:]
-#     return result
-
-# def remove_lowest_values(sorted_list):
-#     # Calculate the index to start slicing from
-#     index_to_remove = len(sorted_list) // 2
-#     # Slice the list to remove the lowest values
-#     result = sorted_list[index_to_remove:]
-#     return result
-
-# population = []
-
-# def get_fitness(game):
-#     return game.fitness
-
-# # Create 100 instances of the Snake game and add them to the population
-
-# # for _ in range(1000):
-# #     game = SnakeGame()
-# #     controller = GAController(game)
-# #     population.append(controller)
-# #     game.run()
 
 
 def print_max(population):
@@ -38,13 +12,14 @@ def print_max(population):
 
 def mate_in_pairs(population):     # Extracting the list of keys to access the creatures in order    keys = list(creatures_dict.keys())          # Iterate over the keys in steps of 2 to get pairsfor i in range(0, len(keys) - 1, 2):         creature1 = creatures_dict[keys[i]]         creature2 = creatures_dict[keys[i+1]]         creature1.mate(creature2)
     babies = []
+
     for i in range(0, len(population) - 1, 2):
         dad = population[i]
         mom = population[i+1]
         baby = dad.model + mom.model
         baby2 = dad.model + mom.model
-        baby.mutate(0.05)
-        baby2.mutate(0.05)
+        baby.mutate(0.08)
+        baby2.mutate(0.08)
         babies.append(baby)
         babies.append(baby2)
     # print(babies)
@@ -52,7 +27,7 @@ def mate_in_pairs(population):     # Extracting the list of keys to access the c
 
 
 class GeneticAlgorithm:
-    def __init__(self, population_size=10, generations=2):
+    def __init__(self, population_size=60, generations=5):
         self.population_size = population_size
         self.generations = generations
         self.population = []
@@ -67,10 +42,39 @@ class GeneticAlgorithm:
         print(len(population))
 
 
+        self.print_fitness_values()
         self.population = population
+
+    def evolve_generations_tournament(self):
+        for _ in range(self.generations):
+            self.print_fitness_values()
+            self.generations = self.generations - 1
+            print(self.generations)
+            new_pop = []
+            # Tournament selection
+            tournament_size = 50  # Define the tournament size
+            while len(new_pop) < self.population_size:
+                # Randomly select individuals for the tournament
+                tournament_participants = random.sample(self.population, tournament_size)
+                # Choose the best individual from the tournament
+                winner = max(tournament_participants, key=lambda x: x.game.fitness)
+                print(winner.game.fitness)
+                new_pop.append(winner)
+            print("LENG", len(new_pop))
+
+            # Mating to fill remaining slots
+            while len(new_pop) < self.population_size:
+                dad = random.choice(new_pop)
+                mom = random.choice(new_pop)
+                baby = dad.model + mom.model
+                baby.mutate(0.08)
+                new_controller = GAController(SnakeGame(), model=baby)
+                new_pop.append(new_controller)
+            self.population = new_pop
 
     def evolve_generations(self):
         for _ in range(self.generations):
+            self.print_fitness_values()
             self.generations = self.generations-1
             print(self.generations)
             new_pop=[]
@@ -88,7 +92,7 @@ class GeneticAlgorithm:
             new_pop = sorted(new_pop, key=lambda x: x.game.fitness, reverse=True)
             print("LENG",len(new_pop))
             new_pop = new_pop[:len(new_pop) // 2]
-            self.print_fitness_values(new_pop)
+
             new_controllers = []
             new_models = mate_in_pairs(new_pop)
             for model in new_models:
@@ -98,14 +102,19 @@ class GeneticAlgorithm:
             print("new models",len(new_models))
             print("new controllers",len(new_controllers))
             #.append when make them contain the same objects
+
+
             new_controllers.extend(new_pop)
+            print()
             self.population = new_controllers
 
     def find_best_controller(self):
         sorted_population = sorted(self.population, key=lambda x: x.game.fitness, reverse=True)
         best_controllers = sorted_population[:30]
         return best_controllers
-    def print_fitness_values(self, population):
+    def print_fitness_values(self):
+        population = sorted(self.population, key=lambda x: x.game.fitness, reverse=True)
+        population = population[:10]
         print("Fitness values of the current population:")
         for i, controller in enumerate(population):
             print(f"Controller {i+1}: Fitness = {controller.game.fitness}")
@@ -115,10 +124,10 @@ class GeneticAlgorithm:
 
 
 if __name__ == '__main__':
-    ga = GeneticAlgorithm(population_size=100, generations=200)
+    ga = GeneticAlgorithm(population_size=100, generations=500)
     ga.initialize_population()
+    ga.print_fitness_values()
     ga.evolve_generations()
-    best_controllers = ga.find_best_controller()
-    for i, controller in enumerate(best_controllers, start=1):
-        print(f"Rank {i}: Fitness = {controller.game.fitness}")
+    ga.print_fitness_values()
+    # ga.evolve_generations_tournament()
     # print("Best Controller Fitness:", best_controller.game.fitness)
